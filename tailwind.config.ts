0efa5577670bee2b408dc46cde0d9b44b0c6e339
gmviana <gmunizviana@gmail.com>
2025-05-21 23:21:46 -0300

    ajustando config do tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,15 @@
+// export default {
+//   content: [
+//     "./src/pages/**/*/.{js, jsx, ts, tsx, mdx}",
+//     "./src/components/**/*/.{js, jsx, ts, tsx, mdx}",
+//     "./sr/app/**/*/.{js, jsx, ts, tsx, mdx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
 import type { Config } from "tailwindcss";
 
 export default {
