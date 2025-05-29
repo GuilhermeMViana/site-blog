ebc6521631c637f3f5ac2b86eac2e713fdfd7b7b
GuilhermeMViana <gmunizviana@gmail.com>
2025-05-29 15:43:56 -0300

    refactor(pastas): refatorando pastas em components e criação de template

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
-import { Button } from "../ui/button";
-import { ActiveLink } from "../active-link/active-live";
-import { Logo } from "../logo";
+import { ActiveLink } from "@/components/active-link/active-live";
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
 
 export const Header = () => {
   return (
     <header className="fixed z-50 top-0 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60">
-      <div className="mx-auto max-w-7xl px-4 sm:px6 lg:px80">
+      <div className="mx-auto max-w-7xl px-4 sm:px6 lg:px8">
         <div className="flex h-16 items-center justify-between">
           <Logo />
 
