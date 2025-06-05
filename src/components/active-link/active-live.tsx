3e5f88f8321c72fb0604cd680188a36eda40e4be
GuilhermeMViana <gmunizviana@gmail.com>
2025-06-05 16:05:04 -0300

    fix(activeLink): ajuste de lógica no href do activeLink

--- src/components/active-link/active-live.tsx
+++ src/components/active-link/active-live.tsx
@@ -11,7 +11,7 @@ export const ActiveLink = ({ children, href, ...rest }: ActiveLinkProps) => {
   const isCurrentPath = router.asPath === href || router.asPath === rest.as;
   return (
     <Link
-      href="/"
+      href={href}
       className={cn(
         "text-action-sm transition-colors hover:text-blue-200",
         isCurrentPath ? "text-blue-200" : "text-gray-100"
