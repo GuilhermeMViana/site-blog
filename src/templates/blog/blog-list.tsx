5086bda6cbfc31237fc3f967726da931e67d63ff
GuilhermeMViana <gmunizviana@gmail.com>
2025-06-26 12:22:11 -0300

    feat(post-list): inserindo lógica de grid para posts

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,6 +1,7 @@
 import { Search } from "@/components/search";
 import { useRouter } from "next/router";
 import { PostCard } from "./components/post-card";
+import { PostList } from "./components/post-list";
 
 export function BlogList() {
   const router = useRouter();
@@ -11,7 +12,7 @@ export function BlogList() {
 
   return (
     <div className="flex flex-col py-24 flex-grow h-full">
-      <section className="">
+      <section className="pb-14">
         <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
           <div className="flex flex-col gap-4 md:px-0">
             <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">
@@ -26,17 +27,43 @@ export function BlogList() {
         </div>
       </section>
 
-      <PostCard
-        slug=""
-        title="Transformando seu negócio em uma loja virtual"
-        description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
-        image="/assets/primeiro-post.png"
-        date="25/06/2025"
-        author={{
-          name: "Aspen Dokidis",
-          avatar: "/customer-01.png",
-        }}
-      />
+      <PostList>
+        <PostCard
+          slug=""
+          title="Transformando seu negócio em uma loja virtual"
+          description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
+          image="/assets/primeiro-post.png"
+          date="25/06/2025"
+          author={{
+            name: "Aspen Dokidis",
+            avatar: "/customer-01.png",
+          }}
+        />
+
+        <PostCard
+          slug=""
+          title="Transformando seu negócio em uma loja virtual"
+          description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
+          image="/assets/primeiro-post.png"
+          date="25/06/2025"
+          author={{
+            name: "Aspen Dokidis",
+            avatar: "/customer-01.png",
+          }}
+        />
+
+        <PostCard
+          slug=""
+          title="Transformando seu negócio em uma loja virtual"
+          description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
+          image="/assets/primeiro-post.png"
+          date="25/06/2025"
+          author={{
+            name: "Aspen Dokidis",
+            avatar: "/customer-01.png",
+          }}
+        />
+      </PostList>
     </div>
   );
 }
